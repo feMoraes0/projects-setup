2df6d4dbe111a27ae3b785b614544e0c1c52ea80
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-09-06 19:05:09 -0300

    Added create and messages functions

--- run/project/create/src/formula/formula.py
+++ run/project/create/src/formula/formula.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 from colored import fg, attr
+import os
 
+def success(message):
+    print("%s ✔ {}%s".format(message) % (fg(3), attr(0)))
+
+def error(message):
+    print("%s ✘ {}%s".format(message) % (fg(2), attr(0)))
+
+def create(project_name, framework):
+    if(framework == 'React'):
+        os.system(f'npx create-react-app {project_name}')
+    elif(framework == 'React Native'):
+        os.system(f'npx react-native init {project_name}')
+    elif (framework == 'NodeJS'):
+        os.system(f'mkdir {project_name} && cd {project_name} && npm init -y')
 
 def is_valid(project_name):
     if(project_name.find(" ") != -1):
@@ -9,6 +23,7 @@ def is_valid(project_name):
 
 def Run(project_name, framework, wanna_run):
     if(is_valid(project_name)):
-        print('is valid')
+        create(project_name, framework)
+        success(f'Project {project_name} created with success!')
     else:
-        print('is not valid')
+        error('Invalid project name - white spaces are not allowed!')
